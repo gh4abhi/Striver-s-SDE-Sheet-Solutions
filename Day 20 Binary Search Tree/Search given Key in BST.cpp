82e7cf802685255d0cf494ece72596d8fda7f187
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-30 20:58:37 +0530

    Added solution of one problem.

---
+++ Day 20 Binary Search Tree/Search given Key in BST.cpp
@@ -0,0 +1,29 @@
+// Problem Link - https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* op = root;
+        while(op)
+        {
+            if(val>op->val)
+                op = op->right;
+            else if(val<op->val)
+                op = op->left;
+            else
+                return op;
+        }
+        return nullptr;
+    }
+};
